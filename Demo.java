62e314b85ebfa7d9532d307823a70e31b08f5299
pkomals <pkomals94@gmail.com>
2024-02-10 00:58:18 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,17 @@
+package shapes;
+import shapes.Shape;
+import shapes.circle.Circle;
+import shapes.quad.Square;	
+class Demo{
+	public static void main(String args[]){
+		Shape s=new Shape();
+		s.area();
+		Circle c=new Circle();
+		c.area();
+		Square sq=new Square();
+		sq.area();
+		
+
+	}
+
+}
